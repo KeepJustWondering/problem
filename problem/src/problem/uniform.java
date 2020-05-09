700f3d6bcf0cb97387f4c32b25b0b0a9d48085f7
KeepJustWondering <wodnjs3428@naver.com>
2020-05-09 22:34:23 +0900

    체육복 문제 uniform.java 생성
    점심시간에 도둑이 들어, 일부 학생이 체육복을 도난당했습니다. 다행히 여벌 체육복이 있는 학생이 이들에게 체육복을 빌려주려
    합니다. 학생들의 번호는 체격 순으로 매겨져 있어, 바로 앞번호의 학생이나 바로 뒷번호의 학생에게만 체육복을 빌려줄 수 있습니다.
    예를 들어, 4번 학생은 3번 학생이나 5번 학생에게만 체육복을 빌려줄 수 있습니다. 체육복이 없으면 수업을 들을 수 없기 때문에
    체육복을 적절히 빌려 최대한 많은 학생이 체육수업을 들어야 합니다.
    
    전체 학생의 수 n, 체육복을 도난당한 학생들의 번호가 담긴 배열 lost, 여벌의 체육복을 가져온 학생들의 번호가 담긴 배열
    reserve가 매개변수로 주어질 때, 체육수업을 들을 수 있는 학생의 최댓값을 return 하도록 solution 함수를
    작성해주세요.
    
    제한사항
    전체 학생의 수는 2명 이상 30명 이하입니다.
    체육복을 도난당한 학생의 수는 1명 이상 n명 이하이고 중복되는 번호는 없습니다.
    여벌의 체육복을 가져온 학생의 수는 1명 이상 n명 이하이고 중복되는 번호는 없습니다.
    여벌 체육복이 있는 학생만 다른 학생에게 체육복을 빌려줄 수 있습니다.
    여벌 체육복을 가져온 학생이 체육복을 도난당했을 수 있습니다. 이때 이 학생은 체육복을 하나만 도난당했다고 가정하며, 남은
    체육복이 하나이기에 다른 학생에게는 체육복을 빌려줄 수 없습니다.

---
+++ problem/src/problem/uniform.java
@@ -0,0 +1,43 @@
+package problem;
+
+public class uniform {
+	
+	public static void main(String[] args) {
+		int n=30;
+		int[] lost = {2,3,5,6,7,8,10,11};
+		int[] reserve = {3,4,5,7,8,9};
+		
+		int plus =0;
+	
+
+		for(int i=0;i<reserve.length;i++) {//같은 번호를 가진 학생 색출
+			for(int j=0;j<lost.length;j++) {
+				if(lost[j]==-1 && reserve[i]==-1){
+                    continue;
+                }
+				if(reserve[i] == lost[j]){
+					reserve[i]=-1;
+					lost[j]=-1;
+					plus++;
+				}
+			}		
+		}
+	
+		
+		for(int i=0;i<reserve.length;i++) {//1차이나는 번호를 가진 학생 색출
+			for(int j=0;j<lost.length;j++) {
+				if(lost[j]==-1 && reserve[i]==-1){
+                    continue;
+                }
+				if(reserve[i]+1 == lost[j]||reserve[i]-1 == lost[j]){//여분을 가진 학생이 빌려줄 사람이 있는 경우
+					reserve[i]=-1;
+					lost[j]=-1;
+					plus++;
+				}
+			}		
+		}
+	
+		
+		System.out.println(n-lost.length+plus);
+	}
+}
