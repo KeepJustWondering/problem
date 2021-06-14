08c474cc63a99214e6c7e7558739a6ab50b2505e
jaewonkim <64457575+jaewonkim@users.noreply.github.com>
2021-06-14 15:21:31 +0900

    그리디알고리즘 거스름돈 문제
    거스름돈으로 사용할 500, 100 ,50 ,10 원이 무한정 존재 할 때 거슬러줘야 할 돈인 10의 배수 N원에 대하여 최소한의 동전을 사용하여 거슬러주는 문제
    
    예시의 N은 1260원으로 결과 6개가 나온다

---
+++ problem/src/problem/changeMoney.java
@@ -0,0 +1,26 @@
+package Greedy;
+
+public class changeMoney {
+
+    public static void main(String[] args){
+
+        int n = 1510;
+        int[] coin_types = {500,100,50,10};
+
+        changeMoney result = new changeMoney();
+        System.out.println(result.Money(n,coin_types));
+
+    }
+    private int Money(int n,int[] ct){
+        int count =0;
+        int index = 0;
+
+        while(n>0){
+            count += n/ct[index];
+            n %= ct[index++];
+        }
+
+        return count;
+    }
+
+}
